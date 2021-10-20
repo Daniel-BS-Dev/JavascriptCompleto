99d92a10226d32f73ba63a12b013ffee3c36101c
Daniel-BS-Dev <danielbenedito355@gmail.com>
2021-10-20 20:59:24 -0300

    Table

--- array/challenge.js
+++ array/challenge.js
@@ -1,3 +1,15 @@
 function sum() {
+    const numbers = Array.from(arguments); // pegando todos os elementos recebidos e transformando em função
+   // const numbres = [...arguments]       // esta seria a outa forma de pegar os valores
+    return numbers.reduce(function(numberSum, currentNumber){
+        return numberSum + currentNumber;
+    },0);
     
 }
+
+function avarege() {
+    return sum(...arguments) / arguments.length;
+}
+
+let s = sum(1,2,3);
+console.log(avarege(1,2,3));
